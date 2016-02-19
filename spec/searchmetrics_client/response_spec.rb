1f83bfa849c7612616e91cd7e51d2872bcdbb15a
Laurent Arnoud <laurent@spkdev.net>
2016-02-19 10:47:18 +0100

    Memoize searchmetrics call on spec to avoid "Insufficient credits"

--- spec/searchmetrics_client/response_spec.rb
+++ spec/searchmetrics_client/response_spec.rb
@@ -2,31 +2,27 @@ require 'spec_helper'
 
 describe SearchmetricsClient::Response, vcr: { cassette_name: 'searchmetrics',
                                                record: :new_episodes } do
-  let(:response) do
-    SearchmetricsClient::Request.send_request_from_hash(
-      endpoint: 'ResearchLinksGetListLinktext', params: { url: 'example.url' }
-    )
-  end
-
   context 'when api credentials are configured' do
     include_context 'with api credentials'
 
+    before :all do
+      VCR.use_cassette('searchmetrics') do
+        @response ||= SearchmetricsClient::Request.send_request_from_hash(
+          endpoint: 'ResearchLinksGetListLinktext', params: { url: 'example.url' }
+        )
+      end
+    end
+
     describe '#value' do
       it 'return hash data' do
-        expect(response.value).to be_instance_of Hash
-        expect(response.value).to eq(balance: 59_981)
+        expect(@response.value).to be_instance_of Hash
+        expect(@response.value).to eq(balance: 59_981)
       end
-    end
-
-    describe '#status' do
       it 'has valid status code' do
-        expect(response.status). to eql(200)
+        expect(@response.status). to eql(200)
       end
-    end
-
-    describe '#request' do
       it 'keeps reference to request' do
-        expect(response.request).to be_instance_of SearchmetricsClient::Request
+        expect(@response.request).to be_instance_of SearchmetricsClient::Request
       end
     end
   end
