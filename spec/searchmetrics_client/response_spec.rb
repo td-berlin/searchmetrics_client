563ddae8a9f8bdc5785bb372569af6d4129e11de
Laurent Arnoud <laurent@spkdev.net>
2016-02-19 11:08:51 +0100

    Fix rubocop offences and add comment for disable

--- spec/searchmetrics_client/response_spec.rb
+++ spec/searchmetrics_client/response_spec.rb
@@ -8,27 +8,33 @@ describe SearchmetricsClient::Response, vcr: { cassette_name: 'searchmetrics',
     before :all do
       VCR.use_cassette('searchmetrics') do
         @response ||= SearchmetricsClient::Request.send_request_from_hash(
-          endpoint: 'ResearchLinksGetListLinktext', params: { url: 'example.url' }
+          endpoint: 'ResearchLinksGetListLinktext',
+          params: { url: 'example.url' }
         )
       end
     end
 
+    # Memoize response to avoid errors
+    # "Insufficient credits to make this service request."
+    # rubocop:disable Style/GlobalVars
+    let(:response) { @response }
+
     describe 'response' do
       it 'returns response when request is correct' do
-        expect(@response).to be_kind_of(SearchmetricsClient::Response)
+        expect(response).to be_kind_of(described_class)
       end
 
       it 'return hash data' do
-        expect(@response.value).to be_instance_of Hash
-        expect(@response.value).to eq(balance: 59_981)
+        expect(response.value).to be_instance_of Hash
+        expect(response.value).to eq(balance: 59_981)
       end
 
       it 'has valid status code' do
-        expect(@response.status). to eql(200)
+        expect(response.status). to eql(200)
       end
 
       it 'keeps reference to request' do
-        expect(@response.request).to be_instance_of SearchmetricsClient::Request
+        expect(response.request).to be_instance_of(SearchmetricsClient::Request)
       end
     end
   end
